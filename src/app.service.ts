39bb83410f8fb410d156699290fd27a0589d1695
josealeuribe <joseuribe.31289@gmail.com>
2026-02-27 10:28:37 -0500

    En el archivo comandos.md, se intancias algunas de las dependencias necesaarias, no hemos empezado con el modelo como tal

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  async testDb() {
+    // Cambia "usuario" por el nombre REAL del model en tu schema.prisma
+    return this.prisma.usuario.findMany({ take: 1 });
   }
 }
